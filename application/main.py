127cf8a7c4453fb424b72cde419eab127333112e
rigal <68374904+CharlesRigal@users.noreply.github.com>
2024-04-24 11:11:39 +0200

    Update main.py

--- application/main.py
+++ application/main.py
@@ -45,4 +45,4 @@ if __name__ == "__main__":
             "workers": number_of_workers(),
             "worker_class": "uvicorn.workers.UvicornWorker",
         }
-        Standalone(app, options).run()
+        StandaloneApplication(app, options).run()
